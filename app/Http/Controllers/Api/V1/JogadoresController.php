39217f9afb2e46e5cf3560d008205e09cba36013
mathfajardo <mathfajardo.contato@hotmail.com>
2025-11-03 17:33:22 -0300

    adicionando autenticacao pelo sanctum

--- app/Http/Controllers/Api/V1/JogadoresController.php
+++ app/Http/Controllers/Api/V1/JogadoresController.php
@@ -26,6 +26,11 @@ class JogadoresController extends Controller
      */
     public function store(Request $request)
     {
+
+        if(!auth()->user()->tokenCan('user-store')) {
+            return $this->error('Unauthorized', 403);
+        }
+
         $validator = Validator::make($request->all(), [
             'nome_jogador' => 'required', 
             'equipe' => 'required',
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\JogadoresController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,9 +10,11 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function (){
     Route::get('/jogadores', [JogadoresController::class, 'index']);
     Route::get('/jogadores/{jogador}', [JogadoresController::class, 'show']);
-    Route::post('/jogadores', [JogadoresController::class, 'store']);
-    Route::put('/jogadores/{jogador}', [JogadoresController::class, 'update']);
-    Route::delete('/jogadores/{jogador}', [JogadoresController::class, 'destroy']);
+    Route::post('/jogadores', [JogadoresController::class, 'store'])->middleware('auth:sanctum');
+    Route::put('/jogadores/{jogador}', [JogadoresController::class, 'update'])->middleware('auth:sanctum');
+    Route::delete('/jogadores/{jogador}', [JogadoresController::class, 'destroy'])->middleware('auth:sanctum');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/teste', [TesteController::class, 'index'])->middleware('auth:sanctum');
 });
 
 
