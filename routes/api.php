d4baff0a897e0d3682e578079abbdd2b4cc7cac4
mathfajardo <mathfajardo.contato@hotmail.com>
2025-11-05 16:24:23 -0300

    ajustes e adição de alguns comentários para maior compreensão da api

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,23 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function (){
+    // rota para trazer todos os jogadores
     Route::get('/jogadores', [JogadoresController::class, 'index']);
+    // rota para passar o id e trazer o jogador especifico
     Route::get('/jogadores/{jogador}', [JogadoresController::class, 'show']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    // rota para adicionar jogadores
     Route::post('/jogadores', [JogadoresController::class, 'store'])->middleware('auth:sanctum');
+    //rota para atualizar jogadores
     Route::put('/jogadores/{jogador}', [JogadoresController::class, 'update'])->middleware('auth:sanctum');
+    // rota para deletar jogadores
     Route::delete('/jogadores/{jogador}', [JogadoresController::class, 'destroy'])->middleware('auth:sanctum');
+
+    // rota para criar token
+    Route::post('/login', [AuthController::class, 'login']);
+    // rota para remover token
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    
+    // rota para criar usuario
     Route::post('/user', [UserController::class, "store"]);
 });
 
